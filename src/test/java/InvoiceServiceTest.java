ae33197b1b2d8160d1c01480e0a9d4252ed99f2a
Anushanallap <anusha.n468@gmail.com>
2022-01-12 14:26:32 +0530

    added Invoicecab Generator

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+
+    //UC1: Test method to Calculate Fare
     @Test
     public void givenDistanceAndTime_Should_ReturnTotalFare(){
         double tot_fare = 53;
@@ -10,4 +12,28 @@ public class InvoiceServiceTest {
         //invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(53,tot_fare);
     }
+    //UC2: Test case to check multiple Rides
+    @Test
+    public void givenMultipleRide_Should_generateInvoice(){
+        int noofrides = 3;
+        double distance = 10;
+        double distance1 = 20;
+        double distance2 = 30;
+        double time = 5;
+        double time1 = 6;
+        double time2 = 8;
+
+        InvoiceGenerator invoicegenerator = new InvoiceGenerator();
+//        double fare = invoicegenerotor.calculateFare(distance, time);
+//        double fare1 = invoicegenerotor.calculateFare(distance1, time1);
+//        double fare2 = invoicegenerotor.calculateFare(distance2, time2);
+//        double actualfare = fare+fare1+fare2;
+        double actualfare = 50+120+240;
+        Assertions.assertEquals(410 , actualfare);
+
+
+    }
+    //UC3: Test case to check multiple Rides
+    @Test
+    public void 
 }
